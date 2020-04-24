5b4f20b0afdad1bb6a343358f3830b638acac651
Yuta Saito <kateinoigakukun@gmail.com>
2020-04-24 14:34:15 +0900

    Add lto test case for static and thin variants

--- test/Driver/link-time-opt-darwin.swift
+++ test/Driver/link-time-opt-darwin.swift
@@ -1,7 +1,22 @@
 // REQUIRES: OS=macosx
-// RUN: %target-swiftc_driver -driver-print-jobs %S/../Inputs/empty.swift -llvm-lto | %FileCheck %s
+// RUN: %target-swiftc_driver -driver-print-jobs %S/../Inputs/empty.swift -lto=llvm | %FileCheck %s
 // CHECK: bin/swift -frontend -emit-bc
 // CHECK-NEXT: bin/ld {{.+}} -lto_library {{.+}}/lib/libLTO.dylib
 
-// RUN: %target-swiftc_driver %S/Inputs/lto/lib.swift -llvm-lto -emit-library -emit-module -module-name A
-// RUN: %target-swiftc_driver %S/Inputs/lto/main.swift -L. -I. -lA -llvm-lto
+// RUN: rm -rf %t
+// RUN: %empty-directory(%t/thin)
+// RUN: %empty-directory(%t/full)
+// RUN: %empty-directory(%t/thin-static)
+// RUN: %empty-directory(%t/full-static)
+
+// RUN: %target-swiftc_driver %S/Inputs/lto/lib.swift -lto=llvm -emit-library -emit-module -module-name A -working-directory %t/thin
+// RUN: %target-swiftc_driver %S/Inputs/lto/main.swift -L. -I. -lA -lto=llvm -working-directory %t/thin
+
+// RUN: %target-swiftc_driver %S/Inputs/lto/lib.swift -lto=llvm-full -emit-library -emit-module -module-name A -working-directory %t/full
+// RUN: %target-swiftc_driver %S/Inputs/lto/main.swift -L. -I. -lA -lto=llvm-full -working-directory %t/full
+
+// RUN: %target-swiftc_driver %S/Inputs/lto/lib.swift -static -lto=llvm -emit-library -emit-module -module-name A -working-directory %t/thin-static
+// RUN: %target-swiftc_driver %S/Inputs/lto/main.swift -L. -I. -lA -lto=llvm -working-directory %t/thin-static
+
+// RUN: %target-swiftc_driver %S/Inputs/lto/lib.swift -static -lto=llvm-full -emit-library -emit-module -module-name A -working-directory %t/full-static
+// RUN: %target-swiftc_driver %S/Inputs/lto/main.swift -L. -I. -lA -lto=llvm-full -working-directory %t/full-static
